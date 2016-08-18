90e471c5c207fb0f98c7711245cd9f0a61f36939
Tomas King <tomas.king@asos.com>
2016-08-18 09:51:40 +0100

    Added unit test project

---
+++ tests/FatFoodie.UnitTests/Application/Recipe/RecipeServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using FatFoodie.Application.Recipe;
+
+using NUnit.Framework;
+
+using Shouldly;
+
+namespace FatFoodie.UnitTests.Application.Recipe
+{
+    public class RecipeServiceTests
+    {
+        private IRecipeService recipeService;
+
+        [SetUp]
+        public void Setup()
+        {
+            recipeService = new RecipeService();
+        }
+
+        [Test]
+        public void TestSomething()
+        {
+            var recipes = recipeService.GetAllRecipes();
+
+            recipes.ShouldNotBeNull();
+        }
+    }
+}
