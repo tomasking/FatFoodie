199467632ec39c016bf4e45e2d327249ba6e25bb
Tomas King <tom@kingme.co.uk>
2016-12-26 18:10:31 +0000

    Started adding end to end tests with local db

---
+++ tests/FatFoodie.EndToEndTests/LocalDb/DatabaseExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.SqlServer.Dac;
+
+namespace FatFoodie.EndToEndTests.LocalDb
+{
+    public class DatabaseExtensions
+    {
+        public static void DeployLocalDb()
+        {
+            string DatabaseName = "FatFoodie";
+            string dacConnectionString =$"Server=(localdb)\\mssqllocaldb; Integrated Security=true; database={DatabaseName}";
+            var dacServices = new DacServices(dacConnectionString);
+            dacServices.Message += (sender, args) => Console.WriteLine($"{args.Message.Prefix}: {args.Message.Message}");
+            var package = DacPackage.Load(@"C:\git\FatFoodie\tests\FatFoodie.EndToEnd\bin\Debug\EndToEnd\LocalDb\FatFoodie.Database.dacpac");
+            dacServices.Deploy(package, DatabaseName, true, new DacDeployOptions()
+            {
+                BlockOnPossibleDataLoss = false
+            });
+        }
+
+        public static void ClearAllTables()
+        {
+            string dacConnectionString = $"Server=(localdb)\\mssqllocaldb; Integrated Security=true; database=FatFoodie";
+
+            using (var conn = new SqlConnection(dacConnectionString))
+            {
+                conn.Open();
+                conn.Execute("DELETE FROM Recipe");
+            }
+        }
+    }
+}
